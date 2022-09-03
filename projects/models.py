03683f1cc05a70bed2528e3ef7e8845f5732eeca
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-09-03 12:56:37 +0200

    project details

--- projects/models.py
+++ projects/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Project(models.Model):
-    title = models.CharField(max_length = 100)
+    title = models.CharField(max_length = 1000)
     description = models.TextField()
-    technology = models.CharField(max_length = 20)
-    image = models.CharField(max_length = 100)
+    technology = models.CharField(max_length = 200)
+    image = models.CharField(max_length = 1000)
+
+ 
 
    
